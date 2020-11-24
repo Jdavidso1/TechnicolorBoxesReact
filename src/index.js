f4979e9ab21adcf2d4f4156afbb451ee0d272b90
James Davidson <james.la.davidson@gmail.com>
2020-11-24 17:32:07 -0500

    first commit of technicolor boxes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class Square extends React.Component 
+{
+    randomColor(event) 
+    {
+        var color = '#';
+        var code = '0123456789ABCDEF';
+        var box = event.target;
+        for (var x = 0; x < 6; x++)
+        {
+            color = color + (code[Math.floor(Math.random() * 16)]);
+        }
+        box.style.backgroundColor = color;
+    }
+    render() 
+    {
+        return <div className="square" onMouseOver={this.randomColor}></div>;
+    }
+}
+
+function Main() {
+    const squares = []
+    for (let x=0; x<25; x++) 
+    {
+        squares.push(<Square/>)
+    }
+    return <div id="main">{squares}</div>;
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Main></Main>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
